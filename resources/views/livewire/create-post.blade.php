f8cff331dadbfa460ec92791ceef1fe673798c17
LeviCanales <desarrollador@sertektechnology.com>
2021-10-28 13:58:57 -0600

    10 Componentes de anidamiento

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -11,35 +11,36 @@
 
         <x-slot name="content">
 
-            <div wire:loading wire:target="image" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <div wire:loading wire:target="image"
+                class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
                 <strong class="font-bold">¡Imagen cargando!</strong>
                 <span class="block sm:inline">Espere un momento hasta que la imagen se haya procesado</span>
-              </div>
+            </div>
 
             @if ($image)
-                <img class="mb-4" src="{{$image->temporaryUrl()}}">
+                <img class="mb-4" src="{{ $image->temporaryUrl() }}">
             @endif
 
             <div class="mb-4">
-                <x-jet-label value="Título del post"/>
+                <x-jet-label value="Título del post" />
                 <x-jet-input type="text" class="w-full" wire:model="title" />
                 {{-- @error('title')
                     <span>
                         {{$message}};
                     </span>
                 @enderror --}}
-                <x-jet-input-error for="title"/>
+                <x-jet-input-error for="title" />
             </div>
 
             <div class="mb-4">
-                <x-jet-label value="Contenido del post"/>
+                <x-jet-label value="Contenido del post" />
                 <textarea wire:model.defer="content" class="form-control w-full" rows="6"></textarea>
-                <x-jet-input-error for="content"/>
+                <x-jet-input-error for="content" />
             </div>
 
             <div>
-                <input type="file" wire:model="image" id="{{$identificador}}">
-                <x-jet-input-error for="image"/>
+                <input type="file" wire:model="image" id="{{ $identificador }}">
+                <x-jet-input-error for="image" />
             </div>
 
         </x-slot>
@@ -49,7 +50,8 @@
                 Cancelar
             </x-jet-secondary-button>
 
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image"
+                class="disabled:opacity-25">
                 Crear Post
             </x-jet-danger-button>
         </x-slot>
